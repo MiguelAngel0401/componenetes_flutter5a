02318eb30fdaa608202e774c594c60019442226a
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2024-03-07 09:52:27 -0600

    RadioButton

--- lib/screens/inputs.dart
+++ lib/screens/inputs.dart
@@ -11,6 +11,7 @@ class Inputs extends StatefulWidget {
 class _InputsState extends State<Inputs> {
   bool valueSwitch = false;
   double sliderValue = 0.0;
+  int foodRadio = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +28,7 @@ class _InputsState extends State<Inputs> {
             entradaTexto(),
             entradaSwitch(),
             entradaSlider(),
+            entradasRadio(),
             const ElevatedButton(
                 onPressed: null,
                 child: Text(
@@ -96,6 +98,47 @@ class _InputsState extends State<Inputs> {
                 sliderValue = value;
               });
             }),
+      ],
+    );
+  }
+
+  Column entradasRadio() {
+    return Column(
+      children: [
+        Text(
+          '¿Que prefiere?',
+          style: AppTheme.lightTheme.textTheme.headlineLarge,
+        ),
+        ListTile(
+          title: Text(
+            'Tacos al pastor',
+            style: AppTheme.lightTheme.textTheme.bodySmall,
+          ),
+          leading: Radio(
+            value: 1,
+            groupValue: foodRadio,
+            onChanged: (value) {
+              setState(() {
+                foodRadio = value!;
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: Text(
+            'Chiles Rellenos',
+            style: AppTheme.lightTheme.textTheme.bodySmall,
+          ),
+          leading: Radio(
+            value: 2,
+            groupValue: foodRadio,
+            onChanged: (value) {
+              setState(() {
+                foodRadio = value!;
+              });
+            },
+          ),
+        ),
       ],
     );
   }
